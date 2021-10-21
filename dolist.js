24179ee0806b2e0e2051e3e0f3b8b213fc5ed6fd
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-21 16:58:12 +0300

    jquery

--- dolist.js
+++ dolist.js
@@ -1,6 +1,32 @@
 $("p").css("color","white")
 $(".page").css("font-size","30px")
 
+// $(".inp1").on ("click" , addfun)
 
 
+
+
+
+
+const toDos = [{name:"eat",iscompleted:false}]
+const render =()=>{
+    toDos.forEach((element,i )=> {
+
+    $(".mylist").append(`<li>${element.name}</li>`)
+
+    })
+    
+
+}
+
+
+render()
+
+
+
+
+addToList ()
+
+
+$("#addBtn").click(addToList)
 
