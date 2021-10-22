1752a97e0f007ba1ca814ee6cd57cb5999b1cece
Thoraya2021 <https://github.com/Thoraya2021/myFirstRepo>
2021-10-22 13:16:30 +0300

    editlist

--- dolist.js
+++ dolist.js
@@ -1,32 +1,62 @@
+
 $("p").css("color","white")
-$(".page").css("font-size","30px")
+$(".page").css("font-size","50px")
 
-// $(".inp1").on ("click" , addfun)
+let list = [];
+$("#addBtn").click(() => {
+  if ($("input[type=text").val().trim().length > 0) {
+    list.push({ item: $("input[type=text").val(), isCompleted: false });
+    renderList();
+  }
+});
 
+const renderList = () => {
+  $("ul").html("");
+  list.forEach((element, i) => {
+    $("ul").append(
+      `<li id='${"li" + i}'><span id='${
+        "allSpan" + i
+      }'><span onclick='completed(${i})' id='${"element" + i}'>${
+        element.item
+      }  </span><span id='ed' onclick='edit(${i})'>Edit</span> <span id='ed' onclick='remove(${i})'>Remove</span> </span></li>`
+    );
+    if (list[i].isCompleted) {
+      $("#element" + i).css("text-decoration", "line-through");
+    }
+  });
+  let count = list.filter((item) => item.isCompleted === false).length;
+  $("#count").text(count);
+};
 
+const edit = (i) => {
+  $("#allSpan" + i).hide();
+  $("#li" + i).append(`<input type='text' id='ed' value='${list[i].item}'>`);
+  $("input#ed").change((e) => {
+    list[i].item = e.target.value;
+    renderList();
+  });
+};
 
+const remove = (i) => {
+  console.log("trigered");
+  list.splice(i, 1);
+  renderList();
+};
 
+const completed = (i) => {
+  list[i].isCompleted = !list[i].isCompleted;
+  renderList();
+};
 
+$(".clear1").click(() => {
+  list = [];
+  renderList();
+});
 
-const toDos = [{name:"eat",iscompleted:false}]
-const render =()=>{
-    toDos.forEach((element,i )=> {
-
-    $(".mylist").append(`<li>${element.name}</li>`)
-
-    })
-    
-
-}
-
-
-render()
-
-
-
-
-addToList ()
-
-
-$("#addBtn").click(addToList)
-
+$(".clear2").click(() => {
+  let completedList = list.filter((item) => {
+    return item.isCompleted === false;
+  });
+  list = completedList;
+  renderList();
+});
